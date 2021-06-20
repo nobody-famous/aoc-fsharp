3df1115bac9ac3c825a7985061246c9a53694f09
Rich Heller <rellehr@gmail.com>
2021-06-19 22:47:38 -0500

    Part 2 solution

--- Runner.fs
+++ Runner.fs
@@ -9,7 +9,8 @@ let problems2018 =
       { Data = (2018, 1, 2, Aoc.Year2018.Day1.Part2.run 655, "puzzle.txt") }
       { Data = (2018, 2, 1, Aoc.Year2018.Day2.Part1.run 6225, "puzzle.txt") }
       { Data = (2018, 2, 1, Aoc.Year2018.Day2.Part2.run "revtaubfniyhsgxdoajwkqilp", "puzzle.txt") }
-      { Data = (2018, 3, 1, Aoc.Year2018.Day3.Part1.run 118858, "puzzle.txt") } ]
+      { Data = (2018, 3, 1, Aoc.Year2018.Day3.Part1.run 118858, "puzzle.txt") }
+      { Data = (2018, 3, 1, Aoc.Year2018.Day3.Part2.run 1100, "puzzle.txt") } ]
 
 // let problems2019 =
 //     [ { data = (2019, 1, 1, 3279287, aoc.year2019.day1.part1.run, "puzzle.txt") }
@@ -31,7 +32,7 @@ let runAll probs =
 [<EntryPoint>]
 let main argv =
     // let problems2018 =
-    //     [ { data = (2018, 3, 1, Aoc.Year2018.Day3.Part1.run 0, "puzzle.txt") } ]
+    //     [ { Data = (2018, 3, 1, Aoc.Year2018.Day3.Part2.run 1100, "puzzle.txt") } ]
 
     runAll problems2018
 
