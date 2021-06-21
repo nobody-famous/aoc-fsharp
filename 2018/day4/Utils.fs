61a1ba5b9b85d2ae519c8534fd405334af8119a4
nobody-famous <38633692+nobody-famous@users.noreply.github.com>
2021-06-20 22:37:06 -0500

    2018 day4 (#9)
    * Parse data
    
    * Part 1 solution
    
    * Code cleanup
    
    * Code cleanup
    
    * Part 2 solution
    
    * Fix runner

---
+++ 2018/day4/Utils.fs
@@ -0,0 +1,15 @@
+module Aoc.Year2018.Day4.Utils
+
+type DateEntry =
+    { Year: int
+      Month: int
+      Day: int
+      Hour: int
+      Minute: int }
+
+type Entry =
+    | StartShift of DateEntry * int
+    | Sleep of DateEntry
+    | Wake of DateEntry
+
+type GuardEntry = { Start: DateEntry; End: DateEntry }
