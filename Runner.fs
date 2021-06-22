f3256ca121c481410a4bac90b33c3cb888bede52
nobody-famous <38633692+nobody-famous@users.noreply.github.com>
2021-06-22 18:30:42 -0500

    2018 day6 (#11)
    * Parsing and initial work for part 1
    
    * Part 1 solution
    
    * Part 2 solution

--- Runner.fs
+++ Runner.fs
@@ -14,7 +14,9 @@ let problems2018 =
       { Data = (2018, 4, 1, Aoc.Year2018.Day4.Part1.run 39698, "puzzle.txt") }
       { Data = (2018, 4, 2, Aoc.Year2018.Day4.Part2.run 14920, "puzzle.txt") }
       { Data = (2018, 5, 1, Aoc.Year2018.Day5.Part1.run 11590, "puzzle.txt") }
-      { Data = (2018, 5, 1, Aoc.Year2018.Day5.Part2.run 4504, "puzzle.txt") } ]
+      { Data = (2018, 5, 2, Aoc.Year2018.Day5.Part2.run 4504, "puzzle.txt") }
+      { Data = (2018, 6, 1, Aoc.Year2018.Day6.Part1.run 3569, "puzzle.txt") }
+      { Data = (2018, 6, 2, Aoc.Year2018.Day6.Part2.run 48978, "puzzle.txt") } ]
 
 // let problems2019 =
 //     [ { data = (2019, 1, 1, 3279287, aoc.year2019.day1.part1.run, "puzzle.txt") }
@@ -36,7 +38,7 @@ let runAll probs =
 [<EntryPoint>]
 let main argv =
     // let problems2018 =
-    //     [ { Data = (2018, 5, 1, Aoc.Year2018.Day5.Part2.run 11590, "puzzle.txt") } ]
+    //     [ { Data = (2018, 6, 2, Aoc.Year2018.Day6.Part2.run 48978, "puzzle.txt") } ]
 
     runAll problems2018
 
--- Utils/Geometry.fs
+++ Utils/Geometry.fs
@@ -8,3 +8,18 @@ let origin = { X = 0; Y = 0 }
 
 let manDist pt1 pt2 =
     abs (pt1.X - pt2.X) + abs (pt1.Y - pt2.Y)
+
+let makePoint x y = { X = x; Y = y }
+
+let findBounds points =
+    Array.fold
+        (fun (minPt, maxPt) pt ->
+            ({ X = (System.Math.Min(minPt.X, pt.X))
+               Y = System.Math.Min(minPt.Y, pt.Y) },
+             { X = System.Math.Max(maxPt.X, pt.X)
+               Y = System.Math.Max(maxPt.Y, pt.Y) }))
+        ({ X = System.Int32.MaxValue
+           Y = System.Int32.MaxValue },
+         { X = System.Int32.MinValue
+           Y = System.Int32.MinValue })
+        points
