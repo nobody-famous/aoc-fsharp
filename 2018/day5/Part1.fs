010eb7c927c87261e48199f71d3a1a6af4d741d0
Rich Heller <rellehr@gmail.com>
2021-06-21 22:17:52 -0500

    Part 2 solution

--- 2018/day5/Part1.fs
+++ 2018/day5/Part1.fs
@@ -1,21 +1,10 @@
 module Aoc.Year2018.Day5.Part1
 
-type ReactRange = { Start: int; End: int }
-
-let isUpperMatch lc ch = System.Char.ToUpper lc = ch
-let isLowerMatch uc ch = System.Char.ToLower uc = ch
-
-let willTrigger unit1 unit2 =
-    match unit1 with
-    | lc when unit1 >= 'a' && unit1 <= 'z' -> isUpperMatch lc unit2
-    | uc when unit1 >= 'A' && unit1 <= 'Z' -> isLowerMatch uc unit2
-    | _ -> false
-
 let runReaction (polymer: string) =
     Seq.fold
         (fun stack ch ->
             match stack with
-            | top :: rest when willTrigger top ch -> rest
+            | top :: rest when Utils.willTrigger top ch -> rest
             | s -> ch :: s)
         []
         polymer
