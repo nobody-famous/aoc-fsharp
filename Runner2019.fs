9ab764f7c1c0c6c51e52d67b896103cefb90383d
Rich Heller <rellehr@gmail.com>
2021-08-13 22:24:48 -0500

    Part 2

--- Runner2019.fs
+++ Runner2019.fs
@@ -5,7 +5,8 @@ module Runner2019
 open Aoc.Utils.Run
 
 let problems2019 =
-    [ { Data = (2019, 1, 1, Aoc.Year2019.Day1.Part1.run 3279287, "puzzle.txt") } ]
+    [ { Data = (2019, 1, 1, Aoc.Year2019.Day1.Part1.run 3279287, "puzzle.txt") }
+      { Data = (2019, 1, 2, Aoc.Year2019.Day1.Part2.run 4916076, "puzzle.txt") } ]
 
 let runAll probs =
     let total =
@@ -15,8 +16,8 @@ let runAll probs =
 
 [<EntryPoint>]
 let main argv =
-    // let problems2018 =
-    //     [ { Data = (2018, 10, 2, Aoc.Year2018.Day10.Part2.run 10054, "puzzle.txt") } ]
+    // let problems2019 =
+    //     [ { Data = (2019, 1, 2, Aoc.Year2019.Day1.Part2.run 4916076, "puzzle.txt") } ]
 
     runAll problems2019
 
