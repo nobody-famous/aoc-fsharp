525ffdbad2b6e197acd1abc75ede990ccb5b6be5
nobody-famous <38633692+nobody-famous@users.noreply.github.com>
2021-12-11 17:48:30 -0600

    Day9 (#22)
    * Part 1 work
    
    * Day 1 & 2, peformance fixes

---
+++ src/2018/day9/Part1.fs
@@ -0,0 +1,4 @@
+﻿module y2018.day9.part1
+
+
+let run (input: string) = utils.parse input |> utils.playGame
---
+++ src/2018/day9/Part2.fs
@@ -0,0 +1,9 @@
+﻿module y2018.day9.part2
+
+let updateLastMarble config =
+    { config with utils.LastMarble = config.LastMarble * 100 }
+
+let run (input: string) =
+    utils.parse input
+    |> updateLastMarble
+    |> utils.playGame
