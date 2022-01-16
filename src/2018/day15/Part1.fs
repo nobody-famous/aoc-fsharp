e2d682286ef02b0d39861804a919efea706bf0c7
Rich Heller <rellehr@gmail.com>
2022-01-15 18:27:18 -0600

    Get attack targets

--- src/2018/day15/Part1.fs
+++ src/2018/day15/Part1.fs
@@ -10,6 +10,7 @@ type Piece =
     | Elf of int
 
 type Grid = System.Collections.Generic.Dictionary<G.Point, Piece>
+type KVP = System.Collections.Generic.KeyValuePair<G.Point, Piece>
 
 let HitPoints = 200
 
@@ -17,8 +18,6 @@ let parseLines (lines: string array) =
     let grid = Grid()
 
     let parseRow y (line: string) =
-        printfn $"LINE {y} {line}"
-
         Seq.iteri
             (fun x ch ->
                 match ch with
@@ -49,9 +48,74 @@ let printGrid (grid: Grid) =
 
         printfn ""
 
+let findUnits (grid: Grid) =
+    grid
+    |> Seq.filter (fun kv ->
+        match kv.Value with
+        | Goblin _
+        | Elf _ -> true
+        | _ -> false)
+
+let readOrder pts =
+    Seq.sortBy (fun (a: G.Point) -> (a.Y * 1000) + a.X) pts
+
+let getToAct grid =
+    grid
+    |> findUnits
+    |> Seq.map (fun item -> item.Key)
+    |> readOrder
+    |> Seq.map (fun pt ->
+        match grid.TryGetValue pt with
+        | true, v -> (pt, v)
+        | _ -> failwith "SHOULD NOT HAPPEN")
+
+let neighborPoints (pt: G.Point) =
+    [ { G.X = pt.X - 1; G.Y = pt.Y }
+      { G.X = pt.X + 1; G.Y = pt.Y }
+      { G.X = pt.X; G.Y = pt.Y - 1 }
+      { G.X = pt.X; G.Y = pt.Y + 1 } ]
+
+let getToAttack (grid: Grid) (item: G.Point * Piece) =
+    let (itemPt, piece) = item
+
+    neighborPoints itemPt
+    |> List.filter (fun pt ->
+        match grid.TryGetValue pt with
+        | true, Goblin _ ->
+            match piece with
+            | Elf _ -> true
+            | _ -> false
+        | true, Elf _ ->
+            match piece with
+            | Goblin _ -> true
+            | _ -> false
+        | _ -> false)
+
+let doAttack grid (targets: G.Point seq) =
+    for target in targets do
+        printfn $"  ATTACK {target.X},{target.Y}"
+
+let doAction grid (item: G.Point * Piece) =
+    let (pt, piece) = item
+    printfn $"{pt.X},{pt.Y} {piece}"
+
+    match getToAttack grid item with
+    | targets when Seq.length targets > 0 -> doAttack grid targets
+    | _ -> printfn "NO ATTACK"
+
+let round grid =
+    let toAct = getToAct grid
+
+    for item in toAct do
+        doAction grid item
+
+    grid
+
 let run (input: string) =
     let grid = parse input
 
     printGrid grid
+
+    round grid |> ignore
 
     0
