6e84c280c652e0df284ecfedeed97fc07ce896b0
Rich Heller <rellehr@gmail.com>
2022-01-31 22:32:20 -0600

    Part 1

--- src/2018/day16/Part1.fs
+++ src/2018/day16/Part1.fs
@@ -4,7 +4,7 @@ module S = Aoc.Utils.String
 
 type RawInstr = int * int * int * int
 
-type State = { Registers: Map<int, int> }
+type State = Map<int, int>
 
 type Sample =
     { Before: State
@@ -15,19 +15,26 @@ type Config =
     { Samples: Sample list
       Prog: RawInstr list }
 
-let newState values = { Registers = Map values }
+let newState values = Map values
 
 let parseInstr (line: string) =
-    let parts = line.Split ' ' |> Array.map int
+    try
+        let parts =
+            line.Split ' '
+            |> Array.map (fun p -> p.Trim() |> int)
 
-    RawInstr(parts.[0], parts.[1], parts.[2], parts.[3])
+        RawInstr(parts.[0], parts.[1], parts.[2], parts.[3])
+    with
+    | ex ->
+        printfn $"FAILED {line}"
+        raise ex
 
 let parseSamples (lines: string array) =
     let parseIntList (line: string) =
         let intList = line.Substring(line.IndexOf '[')
 
         intList.[1..intList.Length - 2].Split ','
-        |> Array.map int
+        |> Array.map (fun p -> p.Trim() |> int)
         |> Array.mapi (fun ndx v -> (ndx, v))
         |> newState
 
@@ -56,50 +63,48 @@ let parseProg (lines: string array) ndx =
 let parse (input: string) =
     let lines = input.Split '\n' |> S.trimIndent
     let (ndx, samples) = parseSamples lines
-    let prog = parseProg lines ndx
+    // let prog = parseProg lines ndx
 
-    { Samples = samples; Prog = prog }
+    { Samples = samples; Prog = [] }
 
-let addr state instr =
-    let (_, a, b, c) = instr
+let rr (state: State) (_, a, b, c) op = state.Add(c, op state.[a] state.[b])
 
-    { state with Registers = state.Registers.Add(c, (state.Registers.[a] + state.Registers.[b])) }
+let ri (state: State) (_, a, b, c) op = state.Add(c, op state.[a] b)
 
-let addi state instr =
-    let (_, a, b, c) = instr
+let ir (state: State) (_, a, b, c) op = state.Add(c, op a state.[b])
 
-    { state with Registers = state.Registers.Add(c, (state.Registers.[a] + b)) }
+let addr state instr = rr state instr (+)
+let addi state instr = ri state instr (+)
 
-let mulr state instr =
-    let (_, a, b, c) = instr
+let mulr state instr = rr state instr (*)
+let muli state instr = ri state instr (*)
 
-    { state with Registers = state.Registers.Add(c, (state.Registers.[a] * state.Registers.[b])) }
+let banr state instr = rr state instr (&&&)
+let bani state instr = ri state instr (&&&)
 
-let muli state instr =
-    let (_, a, b, c) = instr
+let borr state instr = rr state instr (|||)
+let bori state instr = ri state instr (|||)
 
-    { state with Registers = state.Registers.Add(c, (state.Registers.[a] * b)) }
+let setr (state: State) (_, a, _, c) = state.Add(c, state.[a])
+let seti (state: State) (_, a, _, c) = state.Add(c, a)
 
-let banr state instr =
-    let (_, a, b, c) = instr
+let gtir (state: State) (_, a, b, c) =
+    state.Add(c, (if a > state.[b] then 1 else 0))
 
-    { state with Registers = state.Registers.Add(c, (state.Registers.[a] &&& state.Registers.[b])) }
+let gtri (state: State) (_, a, b, c) =
+    state.Add(c, (if state.[a] > b then 1 else 0))
 
-let bani state instr =
-    let (_, a, b, c) = instr
+let gtrr (state: State) (_, a, b, c) =
+    state.Add(c, (if state.[a] > state.[b] then 1 else 0))
 
-    { state with Registers = state.Registers.Add(c, (state.Registers.[a] &&& b)) }
+let eqir (state: State) (_, a, b, c) =
+    state.Add(c, (if a = state.[b] then 1 else 0))
 
-let borr state instr = state
-let bori state instr = state
-let setr state instr = state
-let seti state instr = state
-let gtir state instr = state
-let gtri state instr = state
-let gtrr state instr = state
-let eqir state instr = state
-let eqri state instr = state
-let eqrr state instr = state
+let eqri (state: State) (_, a, b, c) =
+    state.Add(c, (if state.[a] = b then 1 else 0))
+
+let eqrr (state: State) (_, a, b, c) =
+    state.Add(c, (if state.[a] = state.[b] then 1 else 0))
 
 let fns =
     [ addr
@@ -120,17 +125,19 @@ let fns =
       eqrr ]
 
 let countMatches (samples: Sample list) =
-    let mutable count = 0
+    samples
+    |> List.sumBy (fun sample ->
+        let count =
+            fns
+            |> List.sumBy (fun fn ->
+                if fn sample.Before sample.Instr = sample.After then
+                    1
+                else
+                    0)
 
-    for sample in samples do
-        for fn in fns do
-            if (fn (sample.Before) (sample.Instr)) = (sample.After) then
-                count <- count + 1
-
-    count
+        if count >= 3 then 1 else 0)
 
 let run (input: string) =
     let config = parse input
 
-    printfn $"COUNT {countMatches config.Samples}"
-    0
+    countMatches config.Samples
