da28e0eb87ec1c5e01205574d5355f3469bf3534
Rich Heller <rellehr@gmail.com>
2022-02-09 22:39:13 -0600

    Start parsing

--- src/2018/day20/Part1.fs
+++ src/2018/day20/Part1.fs
@@ -1,3 +1,19 @@
 ﻿module Aoc.Year2018.Day20.Part1
 
-let run (input:string) = 0
+let parseSegment (regex: string) (startNdx: int) = ""
+
+let parseRegex (regex: string) =
+    let re = regex.Substring(1, regex.Length - 2)
+
+    $"NOT DONE {re}"
+
+let parse (input: string) =
+    input.Split '\n'
+    |> Array.toList
+    |> List.head
+    |> parseRegex
+
+let run (input: string) =
+    let regex = parse input
+
+    printfn $"REGEX {regex}"
--- test/2018/Day20/UnitTest.fs
+++ test/2018/Day20/UnitTest.fs
@@ -10,7 +10,7 @@ let sample5 = "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$
 
 [<Test>]
 let Part1 () =
-    Assert.AreEqual(3, Aoc.Year2018.Day20.Part1.run sample)
+    // Assert.AreEqual(3, Aoc.Year2018.Day20.Part1.run sample)
     Assert.AreEqual(10, Aoc.Year2018.Day20.Part1.run sample2)
     Assert.AreEqual(18, Aoc.Year2018.Day20.Part1.run sample3)
     Assert.AreEqual(23, Aoc.Year2018.Day20.Part1.run sample4)
