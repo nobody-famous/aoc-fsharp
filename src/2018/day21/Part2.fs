825dbb54401b69e78a6a134247c7a6d95b47f4a2
Rich Heller <rellehr@gmail.com>
2022-02-24 08:52:39 -0600

    More part 2 work

--- src/2018/day21/Part2.fs
+++ src/2018/day21/Part2.fs
@@ -9,13 +9,14 @@ let mach r0 =
     let mutable d = 0
     let mutable e = 0
 
-// #ip 5
+    // #ip 5
 
     d <- 123
+
     while d <> 72 do
         d <- d &&& 456
 
-// 0  seti 123 0 3
+    // 0  seti 123 0 3
 // 1  bani 3 456 3
 // 2  eqri 3 72 3
 // 3  addr 3 5 5
@@ -38,7 +39,7 @@ let mach r0 =
             d <- d * 65899
             d <- d &&& 16777215
 
-// 5  seti 0 9 3
+            // 5  seti 0 9 3
 // 6  bori 3 65536 1
 // 7  seti 9450265 6 3
 // 8  bani 1 255 4
@@ -49,7 +50,7 @@ let mach r0 =
 
             if b <= 256 then
 
-// 13 gtir 256 1 4
+                // 13 gtir 256 1 4
 // 14 addr 4 5 5
 // 15 addi 5 1 5
 // 16 seti 27 1 5
@@ -62,36 +63,37 @@ let mach r0 =
                     c <- e + 1
                     c <- c * 256
 
-// 17 seti 0 9 4
+                    // 17 seti 0 9 4
 // 18 addi 4 1 2
 // 19 muli 2 256 2
 
                     if b <= c then
 
-// 20 gtrr 2 1 2
+                        // 20 gtrr 2 1 2
 // 21 addr 2 5 5
 // 22 addi 5 1 5
 // 23 seti 25 7 5
 
                         e <- e + 1
-                        // goto 18
+                    // goto 18
 
-// 24 addi 4 1 4
+                    // 24 addi 4 1 4
 // 25 seti 17 5 5
 
                     else
                         loop18done <- true
 
                         b <- e
-                // goto 8
+            // goto 18
 
-// 26 setr 4 6 1
-// 27 seti 7 8 5
+            // 24 addi 4 1 4
+// 25 seti 17 5 5
 
             else
                 printfn $"HERE {a} {d}"
                 outerDone <- true
                 loop8done <- true
+
                 if a = d then
                     outerDone <- true
                     loop8done <- true
@@ -101,28 +103,55 @@ let mach r0 =
 // 30 seti 5 8 5
 
 
-
-
-
-let run (input: string) =
+let countInstrs input r0 =
     let state = U.parse input
     let mutable isDone = false
 
     let seen =
         System.Collections.Generic.HashSet<int>()
 
-    mach 986758
+    while not isDone do
+        if state.Mach.Registers.[state.Mach.IpReg] = 28 then
+            if seen.Contains(state.Mach.Registers.[3]) then
+                printfn $"{state.Mach.Registers.[3]}"
+                isDone <- true
+
+            seen.Add(state.Mach.Registers.[3]) |> ignore
+        U.exec state
+
+    state.Mach.Registers.[3]
+
+
+let run (input: string) =
+    let state = U.parse input
+
+    countInstrs input 0
+
+    // printfn $"{countInstrs input 2589}"
+    // printfn $"{countInstrs input 16777107}"
+
+    // let mutable isDone = false
+
+    // let seen =
+    //     System.Collections.Generic.HashSet<int>()
+
+    // mach 986758
+
+    // let mutable low = System.Int32.MaxValue
     // while not isDone do
     //     if state.Mach.Registers.[state.Mach.IpReg] = 28 then
-    //         if seen.Contains(state.Mach.Registers.[3]) then
-    //             isDone <- true
-    //         else
-    //             seen.Add(state.Mach.Registers.[3]) |> ignore
+    //         // if seen.Contains(state.Mach.Registers.[3]) then
+    //         //     isDone <- true
+    //         // else
+    //         //     seen.Add(state.Mach.Registers.[3]) |> ignore
 
-    //         printfn $"{state.Mach.Registers.[3]}"
+    //         if state.Mach.Registers.[3] < low then
+    //             low <- state.Mach.Registers.[3]
+
+    //             printfn $"{low}"
 
     //     U.exec state
 
     // state.Mach.Registers.[3]
 
-    0
+    // 0
