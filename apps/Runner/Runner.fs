53c60f9f7a940c11be738ce1e83154abfde3eea7
Rich Heller <rellehr@gmail.com>
2025-11-07 21:58:08 -0600

    Convert day 14

--- apps/Runner/Runner.fs
+++ apps/Runner/Runner.fs
@@ -3,13 +3,14 @@
 module S = Aoc.Utils.String
 
 let problems: Problem list =
-    [
-      IntProblem("2018/day1/part1", Aoc.Year2018.Day1.Part1.run, 437)
+    [ IntProblem("2018/day1/part1", Aoc.Year2018.Day1.Part1.run, 437)
       IntProblem("2018/day1/part2", Aoc.Year2018.Day1.Part2.run, 655)
       LongProblem("2018/day9/part1", Aoc.Year2018.Day9.Part1.run, 382055)
       LongProblem("2018/day9/part2", Aoc.Year2018.Day9.Part2.run, 3133277384L)
       StringProblem("2018/day13/part1", Aoc.Year2018.Day13.Part1.run, "80,100")
-      StringProblem("2018/day13/part2", Aoc.Year2018.Day13.Part2.run, "16,99") ]
+      StringProblem("2018/day13/part2", Aoc.Year2018.Day13.Part2.run, "16,99")
+      StringProblem("2018/day14/part1", Aoc.Year2018.Day14.Part1.run, "9276422810")
+      IntProblem("2018/day14/part2", Aoc.Year2018.Day14.Part2.run, 20319117) ]
 
 let args = System.Environment.GetCommandLineArgs()
 
---
+++ libs/Years/2018/Day14/Part2.fs
@@ -0,0 +1,43 @@
+﻿module Aoc.Year2018.Day14.Part2
+
+let matches (first: Utils.IntList) startNdx (second: Utils.IntList) =
+    let mutable equal =
+        first.Count >= second.Count && startNdx >= 0
+
+    let mutable sndNdx = 0
+    let endNdx = startNdx + second.Count - 1
+
+    for ndx in startNdx .. endNdx do
+        if equal && first.Item(ndx) <> second.Item(sndNdx) then
+            equal <- false
+
+        sndNdx <- sndNdx + 1
+
+    equal
+
+let rec findRecipes (state: byref<Utils.State>) (target: Utils.IntList) =
+    let ndx = state.Scores.Count - target.Count
+
+    match state with
+    | s when matches state.Scores ndx target -> ndx
+    | s when matches state.Scores (ndx - 1) target -> (ndx - 1)
+    | _ ->
+        let score = Utils.createNewRecipe &state
+        Utils.addNewScore &state score
+        Utils.updateElfs &state
+
+        findRecipes &state target
+
+let buildTarget (input: string list) =
+    let target = Utils.IntList()
+
+    for item in input.[0] do
+        target.Add(int item - int '0')
+
+    target
+
+let run (input: string list) =
+    let target = buildTarget input
+    let mutable state = Utils.newState ()
+
+    findRecipes &state target
