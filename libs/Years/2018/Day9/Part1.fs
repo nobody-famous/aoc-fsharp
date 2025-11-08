44213cc59c8236beeca17233b19acdd178193e14
Rich Heller <rellehr@gmail.com>
2025-11-07 21:43:00 -0600

    Convert day 9

---
+++ libs/Years/2018/Day9/Part1.fs
@@ -0,0 +1,3 @@
+﻿module Aoc.Year2018.Day9.Part1
+
+let run (input: string list) = Utils.parse input |> Utils.playGame
---
+++ libs/Years/2018/Day9/Part2.fs
@@ -0,0 +1,9 @@
+﻿module Aoc.Year2018.Day9.Part2
+
+let updateLastMarble config =
+    { config with Utils.LastMarble = config.LastMarble * 100 }
+
+let run (input: string list) =
+    Utils.parse input
+    |> updateLastMarble
+    |> Utils.playGame
