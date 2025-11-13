f88b8a25647571af183b35b835a9aea8288b2651
Rich Heller <rellehr@gmail.com>
2025-11-12 21:41:55 -0600

    Convert day 1 tests

---
+++ libs/Years/2018.Test/Day1.fs
@@ -0,0 +1,17 @@
+﻿module Aoc.Year2018.Test.Day1
+
+open Xunit
+
+let sample = [ "+1"; "-2"; "+3"; "+1" ]
+
+[<Fact>]
+let Part1 () =
+    let answer = Aoc.Year2018.Day1.Part1.run sample
+
+    Assert.Equal(3, answer)
+
+[<Fact>]
+let Part2 () =
+    let answer = Aoc.Year2018.Day1.Part2.run sample
+
+    Assert.Equal(2, answer)
